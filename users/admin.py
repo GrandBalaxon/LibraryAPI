403b19fccd55e551c59a20416dfd659f5b17d86a
Vlad Spinul <shinokakaov@mail.ru>
2026-08-22 19:17:58 +0300

    feat: registered CustomUser model in admin

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'username', 'role', 'is_staff', 'is_superuser', 'is_active', 'date_joined')
+    search_fields = ('email', 'username')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'role')
+    ordering = ('-date_joined',)
