991bf1e07a41bc5f465acc037a72345ab527fa5d
Vlad Spinul <shinokakaov@mail.ru>
2026-08-23 15:57:04 +0300

    feat: JWT token settings and endpoints

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("user/", include("users.urls"), name="users"),
     path('catalog/', include('catalog.urls'), name='catalog'),
     path('lending/', include('lending.urls'), name='lending'),
+
+    # JWT-эндпоинты
+    path('token/', TokenObtainPairView.as_view(permission_classes=[AllowAny]), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(permission_classes=[AllowAny]), name='token_refresh'),
 
     # drf-spectacular документирование
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
