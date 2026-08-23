d0818159c7eed0cf8079aadde9b1189499b8614f
Vlad Spinul <shinokakaov@mail.ru>
2026-08-23 13:10:35 +0300

    feat: expanded CustomUser model

--- users/models.py
+++ users/models.py
@@ -1,5 +1,32 @@
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+    """Кастомный менеджер для модели пользователя."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('Email обязателен.')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        # Суперпользователь автоматически становится библиотекарем
+        extra_fields.setdefault('role', self.model.Role.LIBRARIAN)
+
+        if not extra_fields.get('is_staff'):
+            raise ValueError('Суперпользователь должен иметь is_staff=True.')
+        if not extra_fields.get('is_superuser'):
+            raise ValueError('Суперпользователь должен иметь is_superuser=True.')
+
+        return self.create_user(email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
@@ -8,6 +35,9 @@ class CustomUser(AbstractUser):
         READER = 'reader', 'Читатель'
         LIBRARIAN = 'librarian', 'Библиотекарь'
 
+    username = None
+    email = models.EmailField(unique=True, verbose_name='Email')
+
     role = models.CharField(
         max_length=20,
         choices=Role.choices,
@@ -15,8 +45,25 @@ class CustomUser(AbstractUser):
         verbose_name='Роль'
     )
 
+    # Персональные данные пользователя
+    first_name = models.CharField(max_length=150, verbose_name='Имя')
+    middle_name = models.CharField(
+        max_length=150,
+        blank=True,
+        null=True,
+        verbose_name='Отчество',
+        help_text='Указать при наличии')
+    last_name = models.CharField(max_length=150, verbose_name='Фамилия')
+    phone_number = models.CharField(max_length=15, verbose_name='Номер телефона')
+    place_of_birth = models.CharField(max_length=150, verbose_name='Город рождения')
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
     def __str__(self):
-        return f'{self.username} ({self.get_role_display()})'
+        return f'{self.email} ({self.get_role_display()})'
 
     class Meta:
         db_table = 'users'
