6cf69d89bb4d13aa86598e58ceea47e8d1b698f7
Vlad Spinul <shinokakaov@mail.ru>
2026-08-23 21:00:51 +0300

    docker: docker_compose.yml file

--- config/settings.py
+++ config/settings.py
@@ -65,7 +65,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
